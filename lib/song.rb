669df526f270dc34a4564a638d280abd31ad9838
Tiffani Best <tiffanimelissabest@gmail.com>
2020-08-06 01:34:49 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,63 @@
+
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@song_count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << @genre
+    @@artists << @artist
+    @@song_count +=1
+  end
+
+  def self.count
+    @@song_count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre| 
+      if genre_count[genre]
+      genre_count[genre] += 1 
+      else
+      genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+      artist_count[artist] +=1
+      else
+      artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+
+end
+
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "Rap")
+halo = Song.new("Halo", "Beyonce", "RB")
+yikes = Song.new("yikes", "Nicki Minaj", "Rap")
+work = Song.new("work", "Rihanna", "RB")
+Song.genres
+p Song.artist_count
+p Song.genre_count
